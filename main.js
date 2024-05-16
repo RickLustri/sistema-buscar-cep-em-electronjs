0bcbe9a405c34de856031f3f29e2a61a1e397270
rkzin <rkzinnine@gmail.com>
2024-05-15 21:52:36 -0300

    Criando Interface do projeto

---
+++ main.js
@@ -0,0 +1,24 @@
+// importando modulo do electron
+const { app, BrowserWindow } = require('electron')
+
+const path = require('path')
+
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+  });
+  
+// função para criar janela do electron
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    })
+  
+    win.loadFile('index.html')
+  }
+
+
+// chamando a função criar janela
+app.whenReady().then(() => {
+    createWindow()
+  })
